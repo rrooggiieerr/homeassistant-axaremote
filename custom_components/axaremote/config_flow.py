88150e29dd22a34f0d711c0f743fc5d1345bab7a
Rogier van Staveren <rogier@batoid.com>
2024-03-14 16:52:35 +0100

    Serial to network bridge support

--- custom_components/axaremote/config_flow.py
+++ custom_components/axaremote/config_flow.py
@@ -1,22 +1,32 @@
 """Config flow for AXA Remote integration."""
+
 from __future__ import annotations
 
 import logging
 import os
-from typing import Any
+from typing import Any, Final
 
 import homeassistant.helpers.config_validation as cv
 import serial
 import serial.tools.list_ports
 import voluptuous as vol
-from axaremote import AXARemote
+from axaremote import AXARemote, AXARemoteTelnet
 from homeassistant import config_entries
+from homeassistant.const import CONF_HOST, CONF_PORT, CONF_TYPE
 from homeassistant.data_entry_flow import FlowResult
 from homeassistant.exceptions import HomeAssistantError
 
-from .const import CONF_MANUAL_PATH, CONF_SERIAL_PORT, DOMAIN
+from .const import (
+    CONF_MANUAL_PATH,
+    CONF_SERIAL_PORT,
+    CONF_TYPE_SERIAL,
+    CONF_TYPE_TELNET,
+    DOMAIN,
+)
 
 _LOGGER = logging.getLogger(__name__)
+
+DEFAULT_PORT: Final = 23
 
 
 class ConfigFlow(config_entries.ConfigFlow, domain=DOMAIN):
@@ -26,11 +36,28 @@ class ConfigFlow(config_entries.ConfigFlow, domain=DOMAIN):
 
     _step_setup_serial_schema = None
 
+    _step_setup_network_schema = vol.Schema(
+        {
+            vol.Required(CONF_HOST): str,
+            vol.Required(CONF_PORT, default=DEFAULT_PORT): int,
+        }
+    )
+
     async def async_step_user(
         self, user_input: dict[str, Any] | None = None
     ) -> FlowResult:
         """Handle the initial step."""
-        return await self.async_step_setup_serial(user_input)
+        if user_input is not None:
+            user_selection = user_input[CONF_TYPE]
+            if user_selection == "Serial":
+                return await self.async_step_setup_serial()
+
+            return await self.async_step_setup_network()
+
+        list_of_types = ["Serial", "Network"]
+
+        schema = vol.Schema({vol.Required(CONF_TYPE): vol.In(list_of_types)})
+        return self.async_show_form(step_id="user", data_schema=schema)
 
     async def async_step_setup_serial(
         self, user_input: dict[str, Any] | None = None
@@ -41,10 +68,9 @@ class ConfigFlow(config_entries.ConfigFlow, domain=DOMAIN):
         ports = await self.hass.async_add_executor_job(serial.tools.list_ports.comports)
         list_of_ports = {}
         for port in ports:
-            list_of_ports[
-                port.device
-            ] = f"{port}, s/n: {port.serial_number or 'n/a'}" + (
-                f" - {port.manufacturer}" if port.manufacturer else ""
+            list_of_ports[port.device] = (
+                f"{port}, s/n: {port.serial_number or 'n/a'}"
+                + (f" - {port.manufacturer}" if port.manufacturer else "")
             )
 
         self._step_setup_serial_schema = vol.Schema(
@@ -82,7 +108,7 @@ class ConfigFlow(config_entries.ConfigFlow, domain=DOMAIN):
 
         Data has the keys from _step_setup_serial_schema with values provided by the user.
         """
-        # Validate the data can be used to set up a connection.
+        # Validate the data can be used to set up a serial connection.
         self._step_setup_serial_schema(data)
 
         serial_port = None
@@ -115,13 +141,81 @@ class ConfigFlow(config_entries.ConfigFlow, domain=DOMAIN):
             _LOGGER.info("Device %s available", serial_port)
         except serial.SerialException as ex:
             raise CannotConnect(
-                f"Unable to connect to the device {serial_port}: {ex}", ex
+                f"Unable to connect to the device {serial_port}"
             ) from ex
 
         # Return info that you want to store in the config entry.
         return {
             "title": f"AXA Remote {serial_port}",
+            CONF_TYPE: CONF_TYPE_SERIAL,
             CONF_SERIAL_PORT: serial_port,
+        }
+
+    async def async_step_setup_network(
+        self, user_input: dict[str, Any] | None = None
+    ) -> FlowResult:
+        """Step when setting up network configuration."""
+        errors: dict[str, str] = {}
+
+        if user_input is not None:
+            try:
+                info = await self.validate_input_setup_network(user_input, errors)
+            except CannotConnect:
+                errors["base"] = "cannot_connect"
+            except Exception as ex:  # pylint: disable=broad-except
+                _LOGGER.exception("Unexpected exception: %s", ex)
+                errors["base"] = "unknown"
+            else:
+                return self.async_create_entry(title=info["title"], data=info)
+            # data = await self.validate_input_setup_network(user_input, errors)
+            # if not errors:
+            #     return self.async_create_entry(
+            #         title=f"{data[CONF_HOST]}:{data[CONF_PORT]}", data=data
+            #     )
+
+        return self.async_show_form(
+            step_id="setup_network",
+            data_schema=self._step_setup_network_schema,
+            errors=errors,
+        )
+
+    async def validate_input_setup_network(
+        self, data: dict[str, Any], errors: dict[str, str]
+    ) -> dict[str, Any]:
+        """Validate the user input allows us to connect.
+
+        Data has the keys from _step_setup_network_schema with values provided by the user.
+        """
+        # Validate the data can be used to set up a network connection.
+        self._step_setup_network_schema(data)
+
+        host = data[CONF_HOST]
+        port = data[CONF_PORT]
+
+        # ToDo Test if the host exists
+
+        await self.async_set_unique_id(f"{host}:{port}")
+        self._abort_if_unique_id_configured()
+
+        # Test if we can connect to the device.
+        try:
+            axa = AXARemoteTelnet(host, port)
+            if not await self.hass.async_add_executor_job(axa.connect):
+                raise CannotConnect(f"Unable to connect to the device on {host}:{port}")
+
+            await self.hass.async_add_executor_job(axa.disconnect)
+            _LOGGER.info("Device on %s:%s available", host, port)
+        except serial.SerialException as ex:
+            raise CannotConnect(
+                f"Unable to connect to the device on {host}:{port}"
+            ) from ex
+
+        # Return info that you want to store in the config entry.
+        return {
+            "title": f"AXA Remote {host}:{port}",
+            CONF_TYPE: CONF_TYPE_TELNET,
+            CONF_HOST: host,
+            CONF_PORT: port,
         }
 
 
